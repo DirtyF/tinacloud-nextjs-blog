ee9e85b289d052062af092604e352c23cc406741
Frank Taillandier <frank@forestry.io>
2021-05-11 14:57:50 +0200

    step: render-preview

--- pages/admin/posts/[slug].js
+++ pages/admin/posts/[slug].js
@@ -1,5 +1,44 @@
-import React from 'react' 
+import React from "react";
+import { useGraphqlForms } from "tina-graphql-gateway";
+import { useRouter } from "next/router";
+import Post from "../../posts/[slug]";
 
-export default function() {
-  return <div>My admin page</div>
+export default function () {
+  const query = (gql) => gql`
+    query BlogPostQuery($relativePath: String!) {
+      getPostsDocument(relativePath: $relativePath) {
+        data {
+          __typename
+          ... on Post_Doc_Data {
+            title
+            excerpt
+            coverImage
+            date
+            author {
+              name
+              picture
+            }
+            ogImage {
+              url
+            }
+            _body
+          }
+        }
+      }
+    }
+  `;
+
+  const router = useRouter();
+  const [payload, isLoading] = useGraphqlForms({
+    query,
+    variables: { relativePath: `${router.query.slug}.md` },
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  const { _body, ...post } = payload.getPostsDocument.data;
+  const pageData = { post, content: _body };
+  return <Post content={pageData.content}{...pageData} />;
 }
